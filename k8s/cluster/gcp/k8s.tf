79f1b6b660383bfbb6b683507d33bd17ddf18fd6
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-05-02 19:05:58 +0900

    auth情報の削除

--- k8s/cluster/gcp/k8s.tf
+++ k8s/cluster/gcp/k8s.tf
@@ -9,32 +9,17 @@ resource "google_container_cluster" "primary" {
 
   remove_default_node_pool = true
   initial_node_count = 1
-
-  master_auth {
-    username = ""
-    password = ""
-
-    client_certificate_config {
-      issue_client_certificate = false
-    }
-  }
 }
 
 resource "google_container_node_pool" "primary_preemptible_node" {
-  name = "demo-node"
+  name = "demo"
   location = local.location
   cluster = google_container_cluster.primary.name
-
-  initial_node_count = 1
   node_count = 1
 
   node_config {
     preemptible = true
     machine_type = local.machine_type
-
-    metadata = {
-      disable-legacy-endpoints = "true"
-    }
 
     oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",

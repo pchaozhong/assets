cf7e2024cf15c200c9f4ddb8b05138c0d6d47f20
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-05-24 14:58:18 +0900

    adding bastion sv and vpn network for searching k8s network

--- k8s/cluster/gcp/k8s.tf
+++ k8s/cluster/gcp/k8s.tf
@@ -10,6 +10,20 @@ resource "google_container_cluster" "primary" {
 
   remove_default_node_pool = true
   initial_node_count = 1
+  network = google_compute_network.gke.self_link
+  subnetwork = google_compute_subnetwork.gke.self_link
+  # cluster_ipv4_cidr = "172.16.0.0/16"
+
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block = "172.16.0.0/16"
+    services_ipv4_cidr_block = "10.10.0.0/16"
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform"
+    ]
+  }
 }
 
 resource "google_container_node_pool" "primary_preemptible_node" {
@@ -23,8 +37,7 @@ resource "google_container_node_pool" "primary_preemptible_node" {
     machine_type = local.machine_type
 
     oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-      "https://www.googleapis.com/auth/monitoring"
+      "https://www.googleapis.com/auth/cloud-platform",
     ]
   }
 }

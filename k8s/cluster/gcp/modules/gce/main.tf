00b062fb21a196f02ca9743e1a52d163654bd2c8
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-06-27 19:26:03 +0900

    [add] gce module

---
+++ k8s/cluster/gcp/modules/gce/main.tf
@@ -0,0 +1,36 @@
+resource "google_compute_instance" "main" {
+  for_each = { for conf in var.gce_confs : conf["name"] =>conf if var.gce_enabled }
+
+  name = each.value["name"]
+  machine_type = each.value["machine_type"]
+  zone = each.value["zone"]
+
+  tags = each.value["tags"]
+
+  network_interface {
+    subnetwork = each.value["subnetwork"]
+    dynamic "access_config" {
+      for_each = { for conf in each.value["access_config"] : conf["conf"] => conf if each.value["access_config_enabled"] }
+
+      content {
+        nat_ip = access_config.value["nat_ip"]
+      }
+    }
+  }
+
+  boot_disk {
+    auto_delete = each.value["boot_disk_auto_delete"]
+    device_name = each.value["boot_disk_device_name"]
+    source = google_compute_disk.boot_disk[each.value["name"]].self_link
+  }
+}
+
+resource "google_compute_disk" "boot_disk" {
+  for_each = { for conf in var.gce_confs : conf["name"] => conf if var.gce_enabled }
+
+  name = "${each.value["name"]}-boot-disk"
+  type = each.value["boot_disk_type"]
+  zone = each.value["zone"]
+  image = each.value["boot_disk_img"]
+  size = each.value["boot_disk_size"]
+}
---
+++ k8s/cluster/gcp/modules/gce/variable.tf
@@ -0,0 +1,24 @@
+variable "gce_enabled" {
+  type    = bool
+  default = true
+}
+
+variable "gce_confs" {
+  type = list(object({
+    name                  = string
+    machine_type          = string
+    zone                  = string
+    tags                  = list(string)
+    subnetwork            = string
+    boot_disk_auto_delete = bool
+    boot_disk_device_name = string
+    boot_disk_type        = string
+    boot_disk_img         = string
+    boot_disk_size        = number
+    access_config_enabled   = bool
+    access_config = list(object({
+      conf = string
+      nat_ip = string
+    }))
+  }))
+}

a0c630d2b45223a7d687491681f2dd24bf337d84
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-08-14 17:39:19 +0900

    [add] sample of sql module

--- terraform/gcp/main.tf
+++ terraform/gcp/main.tf
@@ -86,3 +86,28 @@ module "gce" {
     }
   ]
 }
+
+module "sql" {
+  source = "./modules/sql"
+
+  sql_conf = [
+    {
+      name = "test-db"
+      instance_name = "demo-psgre-proxy"
+    }
+  ]
+
+  db_instance_conf = [
+    {
+      name = "demo-psgre-proxy"
+      db_version = "POSTGRES_11"
+      setting = [
+        {
+          tier = "db-f1-micro"
+          availability_type = "ZONAL"
+          disk_type = "PD_HDD"
+        }
+      ]
+    }
+  ]
+}

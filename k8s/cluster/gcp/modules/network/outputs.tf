b19ece50df3f86b10114fdc1591788e18d145d11
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-08-16 13:26:16 +0900

    [del] gke cluster module の削除

--- k8s/cluster/gcp/modules/network/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "network_self_link" {
-  value = var.vpc_enabled ? google_compute_network.main[0].self_link : null
-}
-
-output "subnetwork_self_links" {
-  value = var.vpc_enabled ? { for k, v in google_compute_subnetwork.main : k => v["self_link"] } : null
-}

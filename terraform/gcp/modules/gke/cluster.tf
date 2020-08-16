ac95705ad829e06225202d11e397eb7ebc5fac64
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-08-16 13:26:37 +0900

    [add] gke moduleの追加

---
+++ terraform/gcp/modules/gke/cluster.tf
@@ -0,0 +1,38 @@
+locals {
+  _cluster_conf = flatten([
+    for _conf in var.gke_conf : [
+      for _cluster in _conf.cluster : {
+        name                      = _conf.cluster_name
+        network                   = _cluster.network
+        location                  = _cluster.location
+        subnetwork                = _cluster.subnetwork
+        node_config               = _cluster.node_config
+        initial_node_count        = _cluster.initial_node_count
+        ip_allocation_policy      = _cluster.ip_allocation_policy
+        remove_default_node_pool  = _cluster.remove_default_node_pool
+        default_max_pods_per_node = _cluster.default_max_pods_per_node
+      }
+    ]
+  ])
+}
+
+resource "google_container_cluster" "main" {
+  for_each = { for v in local._cluster_conf : v.name => v }
+
+  name                      = each.value.name
+  network                   = each.value.network
+  location                  = each.value.location
+  subnetwork                = each.value.subnetwork
+  initial_node_count        = each.value.initial_node_count
+  remove_default_node_pool  = each.value.remove_default_node_pool
+  default_max_pods_per_node = each.value.default_max_pods_per_node
+
+  ip_allocation_policy {
+    cluster_ipv4_cidr_block  = each.value.ip_allocation_policy.cluster_ipv4_cidr_block
+    services_ipv4_cidr_block = each.value.ip_allocation_policy.services_ipv4_cidr_block
+  }
+
+  node_config {
+    oauth_scopes = each.value.node_config.oauth_scopes
+  }
+}
---
+++ terraform/gcp/modules/gke/variable.tf
@@ -0,0 +1,35 @@
+variable "gke_conf" {
+  type = list(object({
+    cluster_name = string
+
+    cluster = list(object({
+      network                   = string
+      location                  = string
+      subnetwork                = string
+      initial_node_count        = number
+      remove_default_node_pool  = bool
+      default_max_pods_per_node = number
+
+      ip_allocation_policy = object({
+        cluster_ipv4_cidr_block  = string
+        services_ipv4_cidr_block = string
+      })
+
+      node_config = object({
+        oauth_scopes = list(string)
+      })
+    }))
+
+    node_pool = list(object({
+      name       = string
+      location   = string
+      node_count = number
+      node_config = object({
+        preemptible  = bool
+        machine_type = string
+        oauth_scopes = list(string)
+      })
+    }))
+
+  }))
+}

0942d312c3668014a0168a1fa3ad0dcd645c1135
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-08-17 10:55:42 +0900

    [mod] terraform fmtの実行

--- terraform/gcp/samples/armor_sample.tf
+++ terraform/gcp/samples/armor_sample.tf
@@ -7,12 +7,12 @@ module "armor" {
     file_path      = "../files/monitoring_ip_list.json"
     file_type      = "json"
     priority_base  = 1000
-    ip_prefix = "ipAddress"
+    ip_prefix      = "ipAddress"
   }
 
   armor_conf = [
     {
-      armor_enable = true
+      armor_enable = false
       name         = "monitoring"
       default_rule = {
         action         = "deny(403)"

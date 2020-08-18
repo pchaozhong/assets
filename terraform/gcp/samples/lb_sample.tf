b62ec9d182837954a648ac62e36e8dd9baad8d33
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-08-19 07:39:05 +0900

    [mod] lb sampleの実行の無効化

--- terraform/gcp/samples/lb_sample.tf
+++ terraform/gcp/samples/lb_sample.tf
@@ -1,88 +1,88 @@
-module "lb" {
-  source = "../modules/lb"
+# module "lb" {
+#   source = "../modules/lb"
 
-  lb_conf = [
-    {
-      instance_template      = "test"
-      instance_group_manager = "test"
-      https_proxy            = "test"
-      url_map                = "test"
-      zone                   = "asia-northeast1-b"
+#   lb_conf = [
+#     {
+#       instance_template      = "test"
+#       instance_group_manager = "test"
+#       https_proxy            = "test"
+#       url_map                = "test"
+#       zone                   = "asia-northeast1-b"
 
-      bk_service = [
-        {
-          name          = "test"
-          health_checks = []
-          protocol      = string
-          timeout_sec   = number
-        }
-      ]
+#       bk_service = [
+#         {
+#           name          = "test"
+#           health_checks = []
+#           protocol      = string
+#           timeout_sec   = number
+#         }
+#       ]
 
-      forwarding_rule = [
-        {
-          name       = string
-          target     = string
-          port_range = string
-          ip_address = string
-        }
-      ]
+#       forwarding_rule = [
+#         {
+#           name       = string
+#           target     = string
+#           port_range = string
+#           ip_address = string
+#         }
+#       ]
 
-      grp_mng = [
-        {
-          base_instance_name = string
-          target_size        = 1
-        }
-      ]
+#       grp_mng = [
+#         {
+#           base_instance_name = string
+#           target_size        = 1
+#         }
+#       ]
 
-      health_checks = [
-        {
-          name = "test"
-          https_health_check = [
-            {
-              request_path = "/"
-              port         = "80"
-            }
-          ]
-          http_health_check = []
-        }
-      ]
+#       health_checks = [
+#         {
+#           name = "test"
+#           https_health_check = [
+#             {
+#               request_path = "/"
+#               port         = "80"
+#             }
+#           ]
+#           http_health_check = []
+#         }
+#       ]
 
-      tgt_https_proxy = [
-        {
-          ssl_certs = [
-            ""
-          ]
-        }
-      ]
+#       tgt_https_proxy = [
+#         {
+#           ssl_certs = [
+#             ""
+#           ]
+#         }
+#       ]
 
-      instance_template = [
-        {
-          machine_type = "f1-micro"
-          tags         = []
-          source_image = ""
-          auto_delete  = true
-          subnetwork   = ""
-          access_config = [
-            {
-              enable = true
-            }
-          ]
-          create_before_destroy = true
-        }
-      ]
+#       instance_template = [
+#         {
+#           machine_type = "f1-micro"
+#           tags         = []
+#           source_image = ""
+#           auto_delete  = true
+#           subnetwork   = ""
+#           access_config = [
+#             {
+#               enable = true
+#             }
+#           ]
+#           create_before_destroy = true
+#         }
+#       ]
 
-      mng_ssl_cert = [
-        {
-          name    = "test"
-          domains = ""
-        }
-      ]
+#       mng_ssl_cert = [
+#         {
+#           name    = "test"
+#           domains = ""
+#         }
+#       ]
 
-      url_map = [
-        {
-          default_service = ""
-        }
-      ]
-    }
-  ]
-}
+#       url_map = [
+#         {
+#           default_service = ""
+#         }
+#       ]
+#     }
+#   ]
+# }

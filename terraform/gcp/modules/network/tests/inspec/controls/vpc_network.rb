dfbb8c16144838f42d4830d15e4ccb0d47be6196
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-08-22 22:51:56 +0900

    [mod] 誤ったfirewallテストの削除

--- terraform/gcp/modules/network/tests/inspec/controls/vpc_network.rb
+++ terraform/gcp/modules/network/tests/inspec/controls/vpc_network.rb
@@ -12,7 +12,4 @@ control 'test' do
     its('ip_cidr_range') { should eq '192.168.0.0/29' }
     its('log_config.enable') { should be false}
   end
-  describe google_compute_firewall(project: gcp_project_id, name: 'test') do
-    it { should_not network 'https://www.googleapis.com/compute/v1/projects/ca-kitano-study-sandbox/global/networks/test'}
-  end
 end

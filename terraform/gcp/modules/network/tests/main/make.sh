a8d5abaa88a4d801feed185af41c28dd15844bcc
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-08-23 07:48:01 +0900

    [mod] Terraformバージョンを外出し変数に変更

--- terraform/gcp/modules/network/tests/main/make.sh
+++ terraform/gcp/modules/network/tests/main/make.sh
@@ -2,10 +2,11 @@
 
 PJ=$1
 PREFIX=$2
+TERRAFORMVERSION=$3
 
 tee terraform.tf <<EOF > /dev/null
 terraform {
-    required_version = "~> 0.13"
+    required_version = "~> $TERRAFORMVERSION"
     backend "gcs" {
         bucket = "$PJ-terraform-modules-state"
         prefix = "$PREFIX"

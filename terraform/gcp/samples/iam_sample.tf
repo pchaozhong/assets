83631045ed01f97f1c60c88d5578d1f445a2f514
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-08-25 09:48:32 +0900

    [mod] file名を正しいものに変更

--- terraform/gcp/samples/iam_sample.tf
+++ terraform/gcp/samples/iam_sample.tf
@@ -2,11 +2,11 @@ module "iam" {
   depends_on = [ module.service_account ]
   source = "../modules/iam"
 
-  iam_member_conf = yamldecode(file("./files/iam_sample.yaml"))
+  iam_member_conf = yamldecode(file("./files/iam.yaml"))
 }
 
 module "service_account" {
   source = "../modules/service_account"
 
-  service_account_conf = yamldecode(file("./files/service_account_sample.yaml"))
+  service_account_conf = yamldecode(file("./files/service_account.yaml"))
 }

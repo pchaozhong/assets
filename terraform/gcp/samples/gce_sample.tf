1550d9cac821136a4a4ca568320c4a2599d278ab
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-08-25 09:49:37 +0900

    [mod] gceサンプルの設定ファイルを外出しに変更

--- terraform/gcp/samples/gce_sample.tf
+++ terraform/gcp/samples/gce_sample.tf
@@ -6,33 +6,6 @@ module "gce" {
   ]
   source     = "../modules/gce"
 
-  gce_conf = [
-    {
-      gce_enable         = false
-      preemptible_enable = true
-
-      name         = "test"
-      machine_type = "f1-micro"
-      zone         = local.zone
-      region       = local.region
-      tags         = ["test"]
-      network      = local.subnetwork.name
-      access_config = {
-        nat_ip = null
-      }
-
-      boot_disk = {
-        size        = 10
-        image       = "ubuntu-2004-lts"
-        type        = "pd-ssd"
-        auto_delete = true
-      }
-      service_account = {
-        email = "module-sample"
-        scopes = [
-          "cloud-platform"
-        ]
-      }
-    }
-  ]
+  gce_conf = yamldecode(file("./files/gce.yaml"))
 }
+

26b6ec80e8b9d2c17f6cbb80d34a82fc67f8ff83
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-08-25 09:45:04 +0900

    [mod] 設定ファイルを外出しに変更

--- terraform/gcp/samples/iam_sample.tf
+++ terraform/gcp/samples/iam_sample.tf
@@ -2,27 +2,11 @@ module "iam" {
   depends_on = [ module.service_account ]
   source = "../modules/iam"
 
-  iam_member_conf = [
-    {
-      iam_enable = false
-
-      member = "module-sample"
-      member_type = "serviceAccount"
-      role = [
-        "roles/owner"
-      ]
-    },
-  ]
+  iam_member_conf = yamldecode(file("./files/iam_sample.yaml"))
 }
-
 
 module "service_account" {
   source = "../modules/service_account"
 
-  service_account_conf = [
-    {
-      service_account_enable = false
-      account_id             = "module-sample"
-    }
-  ]
+  service_account_conf = yamldecode(file("./files/service_account_sample.yaml"))
 }

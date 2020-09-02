8da6a0b77a07cc9c4c56cbbb7ecfe8471b14431a
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-09-02 09:18:24 +0900

    [add] cron_job_sample

---
+++ terraform/gcp/samples/cron_job.tf
@@ -0,0 +1,37 @@
+module "cron_job" {
+  source = "../modules/cron_job"
+
+  cron_job_conf = [
+    {
+      enable = false
+      region = "asia-northeast1"
+
+      scheduler_job_conf = {
+        name      = "sample"
+        time_zone = "Japan"
+        schedule  = "0 22 * * *"
+        data      = "stop+gce"
+        opt_var = {
+        }
+      }
+
+      pubsub_topic_conf = {
+        name    = "sample"
+        opt_var = {}
+      }
+
+      function_conf = {
+        name = "sample"
+        url = join("/", [
+          "https://source.developers.google.com/projects",
+          terraform.workspace,
+          "repos/sample-cron-job/moveable-aliases/master/paths/src",
+        ])
+        runtime     = "go111"
+        entry_point = ""
+        opt_var     = {}
+      }
+
+    }
+  ]
+}

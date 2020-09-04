1d37643620bd33dcd4e44adbf9ee04472e11c080
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-09-05 08:54:46 +0900

    [mod] サンプルコードの修正

--- iaas/sdk/gcp/golang/functions/helloworld/go.mod
+++
@@ -1,3 +0,0 @@
-go 1.12
-
-require google.golang.org/api v0.15.0
--- iaas/sdk/gcp/golang/functions/helloworld/hello_pubsub.go
+++ iaas/sdk/gcp/golang/functions/helloworld/hello_pubsub.go
@@ -3,28 +3,17 @@ package helloworld
 import (
     "context"
     "log"
-    "encoding/json"
 )
-//Pub/Subからのメッセージは"data"にバイト列で入るらしい。構造体のバインドをもっと上手に
-//やる方法を模索中
+
 type PubSubMessage struct {
     Data []byte `json:"data"`
 }
-type Info struct {
-    Name  string `json:"name"`
-    Place string `json:"place"`
-}
-// Pub/Subからメッセージを受診したらこの関数が起動する
-func HelloFromPubSub(ctx context.Context, m PubSubMessage) error {
-    var i Info
 
-    err := json.Unmarshal(m.Data, &i)
-
-    if err != nil {
-        log.Printf("Error:%T message: %v", err, err)
-        return nil
+func HelloPubSub(ctx context.Context, m PubSubMessage) error {
+    name := string(m.Data) // Automatically decoded from base64.
+    if name == "" {
+        name = "World"
     }
-
-    log.Printf("こんにちは、%sさん！%sへCloud Pub/SubからFunctions経由で愛をこめて。", i.Name, i.Place)
+    log.Printf("Hello, %s!", name)
     return nil
 }

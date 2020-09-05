7b77c7da0b8dd2e6c5f48e69a69c573c7dbcd35d
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-09-05 10:02:04 +0900

    [mod] update glue module sample

--- terraform/gcp/samples/glue.tf
+++ terraform/gcp/samples/glue.tf
@@ -1,18 +1,47 @@
+locals {
+  code_repo = "asset"
+  code_file_path = "iaas/sdk/gcp/golang/functions/helloworld/"
+}
+
 module "gcf" {
-  source = "../modules/gcf"
+  source = "../modules/glue"
 
   glue_conf = [
     {
       enable = false
 
       gcf_conf = {
-        name = ""
-        runtime = ""
+        name                  = "HelloWorld"
+        runtime               = "go113"
+        region                = "asia-northeast1"
         environment_variables = {}
-        opt_var = {}
+
+        opt_var = {
+          event_trigger     = true
+          source_repository = true
+
+          entry_point = "HelloPubSub"
+          resource_type = "pubsub"
+          resource      = "module-test"
+          url = join("/", [
+            "https://source.developers.google.com/projects",
+            terraform.workspace,
+            "repos",
+            local.code_repo,
+            "moveable-aliases/master/paths/",
+            local.code_file_path
+          ])
+        }
       }
 
-      pubsub_conf = []
+      pubsub_conf = [
+        {
+          enable = true
+
+          name     = "module-test"
+          opt_conf = {}
+        }
+      ]
       gcs_conf = []
     }
   ]

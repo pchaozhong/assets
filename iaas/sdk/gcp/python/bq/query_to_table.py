3be0b2409a6904ca5144a18c7cb22da1070509a8
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-09-28 11:46:27 +0900

    [mod] change pj name to environment

--- iaas/sdk/gcp/python/bq/query_to_table.py
+++ iaas/sdk/gcp/python/bq/query_to_table.py
@@ -1,7 +1,9 @@
 from google.cloud import bigquery
+import os
 
+pj_name = os.environ["GCP_PROJECT"]
 sql_file = "./files/github_sample.sql"
-table_id = "ca-kitano-study-sandbox.github_source_data.git_sample"
+table_id = pj_name + ".github_source_data.git_sample"
 
 client = bigquery.Client()
 job_config = bigquery.QueryJobConfig(destination=table_id)

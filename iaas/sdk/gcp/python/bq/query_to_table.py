ef482f1f1fa8ba4317b3d7f15d22656f490b89fc
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-09-28 11:51:01 +0900

    [mod] table name and dataset name to variable

--- iaas/sdk/gcp/python/bq/query_to_table.py
+++ iaas/sdk/gcp/python/bq/query_to_table.py
@@ -3,7 +3,10 @@ import os
 
 pj_name = os.environ["GCP_PROJECT"]
 sql_file = "./files/github_sample.sql"
-table_id = pj_name + ".github_source_data.git_sample"
+dataset_name = "github_source_data"
+table_name = "git_sample"
+
+table_id = pj_name + "." + dataset_name + "." + table_name
 
 client = bigquery.Client()
 job_config = bigquery.QueryJobConfig(destination=table_id)

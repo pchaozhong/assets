09b7c8fdb6f202296cdfa3b628fbe94e76932af0
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-09-30 10:50:44 +0900

    [mod] apply terraform fmt

--- terraform/gcp/modules/gce/variable.tf
+++ terraform/gcp/modules/gce/variable.tf
@@ -1,17 +1,17 @@
 variable "gce_conf" {
   type = list(object({
-    gce_enable         = bool
-    name               = string
-    machine_type       = string
-    zone               = string
-    region             = string
-    tags               = list(string)
-    subnetwork         = string
+    gce_enable   = bool
+    name         = string
+    machine_type = string
+    zone         = string
+    region       = string
+    tags         = list(string)
+    subnetwork   = string
     boot_disk = object({
-      size        = number
-      type        = string
-      image       = string
-      opt_conf    = map(string)
+      size     = number
+      type     = string
+      image    = string
+      opt_conf = map(string)
     })
     service_account = object({
       email  = string

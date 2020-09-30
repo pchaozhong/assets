bdabf2093dda738786729120c68cbf10391c7b04
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-09-30 10:46:24 +0900

    [mod] change enable variable name

--- terraform/gcp/modules/service_account/main.tf
+++ terraform/gcp/modules/service_account/main.tf
@@ -2,23 +2,24 @@ locals {
   _service_account_conf = flatten([
     for _conf in var.service_account_conf : {
       account_id = _conf.account_id
-    } if _conf.service_account_enable
+    } if _conf.enable
   ])
 }
 
 variable "service_account_conf" {
   type = list(object({
-    service_account_enable = bool
+    enable = bool
+
     account_id = string
   }))
 }
 
 output "service_accout_email" {
-  value =  { for v in google_service_account.main : v.account_id => v.email} 
+  value = { for v in google_service_account.main : v.account_id => v.email }
 }
 
 resource "google_service_account" "main" {
-  for_each = { for v in local._service_account_conf : v.account_id => v}
+  for_each = { for v in local._service_account_conf : v.account_id => v }
 
-  account_id   = each.value.account_id
+  account_id = each.value.account_id
 }

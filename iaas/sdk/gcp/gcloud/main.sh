9b39552b49fe50c832fb17de172297008d9e0cd4
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-10-14 08:10:16 +0900

    [mod] using common modules

--- iaas/sdk/gcp/gcloud/main.sh
+++ iaas/sdk/gcp/gcloud/main.sh
@@ -1,20 +1,19 @@
 #!/bin/sh
 
 PROJECTS=( \
-         )
+    )
+
 OUTPATH=$1
 LOGDIR=(json csv)
 
-if [ ! -d $OUTPATH ]; then
-    mkdir $OUTPATH
-fi
+source common/make_output_dir.sh
 
-for dir in ${LOGDIR[@]};do
-    mkdir $OUTPATH/$dir
+for dir in ${LOGDIR[@]}; do
+    make_log_parent $OUTPATH $dir
 done
 
 for script in $(ls modules); do
     for pj in ${PROJECTS[@]}; do
-        sh modules/$script $pj $OUTPATH
+        sh modules/$script $pj $OUTPATH > /dev/null
     done
 done

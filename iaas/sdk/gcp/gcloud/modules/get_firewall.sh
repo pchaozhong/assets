30853448ab5c93e5caf46b9000e8be17e3b5b80b
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-10-22 15:38:10 +0900

    [mod] add making csv file at getting firewall config

--- iaas/sdk/gcp/gcloud/modules/get_firewall.sh
+++ iaas/sdk/gcp/gcloud/modules/get_firewall.sh
@@ -3,6 +3,7 @@
 PROJECT=$1
 OUTPUTDIR=$2
 OUTPUT=firewall
+CSVHEADER="name,project,network,direction,priority,logConfig,disabled"
 MODULEPATH="./common/"
 
 SERVICES=(
@@ -24,7 +25,11 @@ for sv in ${SERVICES[@]}; do
 done
 
 make_raw_log_dir $OUTPUTDIR $OUTPUT
+make_header $CSVHEADER $OUTPUTDIR $OUTPUT
 
 for fw in $(gcloud compute firewall-rules list --project $PROJECT | awk 'NR>1{print $1}') ; do
-    gcloud compute firewall-rules describe --format=json --project $PROJECT $fw > $OUTPUTDIR/json/firewall/$fw-$PROJECT.json
+    gcloud compute firewall-rules describe --format=json --project $PROJECT $fw |\
+        tee $OUTPUTDIR/json/firewall/$fw-$PROJECT.json | \
+        jq -c -r '[.name,"'$PROJECT'",.network,.direction,.priority,.logConfig.enable,.disabled]|@csv' |\
+        sed -e 's/"//g' >> $OUTPUTDIR/csv/$OUTPUT.csv
 done

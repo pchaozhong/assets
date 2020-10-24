741ca74352fdf709f7c8450a375b53b6e66ac8ee
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-10-25 07:17:04 +0900

    [mod] instance templateの出力ヘッダの修正

--- iaas/sdk/gcp/gcloud/modules/get_instance_template.sh
+++ iaas/sdk/gcp/gcloud/modules/get_instance_template.sh
@@ -3,7 +3,7 @@
 PROJECT=$1
 OUTPUTDIR=$2
 OUTPUT=instance_template
-CSVHEADER="name,project,machinetype"
+CSVHEADER="name,project,.properties.disks.autoDelete,.properties.disks.boot,.properties.disks.deviceName,.properties.disks.initializeParams.diskSizeGb,.properties.disks.initializeParams.diskType,.properties.disks.initializeParams.sourceImage,.properties.disks.mode,.properties.disks.type,.properties.machineType,.properties.networkInterfaces.network,.properties.networkInterfaces.subnetwork,.properties.scheduling.automaticRestart,.properties.scheduling.onHostMaintenance,.properties.scheduling.preemptible,.properties.serviceAccounts.email,.properties.serviceAccounts.scopes,.properties.tags.items"
 MODULEPATH="./common/"
 
 SERVICES=(
@@ -32,6 +32,6 @@ instance_templates_list=$(gcloud compute instance-templates list --project $PROJ
 for it in ${instance_templates_list[@]}; do
     gcloud compute instance-templates describe --project $PROJECT --format json $it |\
         tee $OUTPUTDIR/json/$OUTPUT/$it-$PROJECT.json |\
-        jq -r -c '[.name,"'$PROJECT'",.properties.machineType]|@csv' |\
+        jq -r -c '[.name,"'$PROJECT'",.properties.disks[].autoDelete,.properties.disks[].boot,.properties.disks[].deviceName,.properties.disks[].initializeParams.diskSizeGb, .properties.disks[].initializeParams.diskType,.properties.disks[].initializeParams.sourceImage,.properties.disks[].mode,.properties.disks[].type,.properties.machineType,.properties.networkInterfaces[].network,.properties.networkInterfaces[].subnetwork,.properties.scheduling.automaticRestart,.properties.scheduling.onHostMaintenance,.properties.scheduling.preemptible,.properties.serviceAccounts[].email,.properties.serviceAccounts[].scopes[],.properties.tags.items[]] |@csv'|\
         sed -e 's/"//g' >> $OUTPUTDIR/csv/$OUTPUT.csv
 done

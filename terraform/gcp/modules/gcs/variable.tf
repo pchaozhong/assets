9e0cfa37a3bc0818d3cbe12d8d7128f502270ab3
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-11-14 13:29:21 +0900

    [mod] gcs moduleのvariableの修正および不要なfileの削除

--- terraform/gcp/modules/gcs/variable.tf
+++ terraform/gcp/modules/gcs/variable.tf
@@ -1,28 +1,90 @@
-variable "gcs_conf" {
+variable "bucket" {
   type = object({
-    enable = bool
-
-    storage_conf = list(object({
-      enable = bool
-
-      name               = string
-      location           = string
-      force_destroy      = string
-      bucket_policy_only = string
-
-      lifecycle_rule = object({
-        enable = bool
-        age    = string
-        type   = string
-      })
-
-      object_conf = list(object({
-        enable   = bool
-        name     = string
-        source   = string
-        opt_conf = map(string)
-      }))
-
-    }))
+    name     = string
+    location = string
   })
+}
+
+
+/*
+Option Configurations
+*/
+
+variable "objects" {
+  type = list(object({
+    name   = string
+    source = string
+  }))
+  default = []
+}
+
+variable "cache_control" {
+  type    = map(string)
+  default = null
+}
+
+variable "content_disposition" {
+  type    = map(string)
+  default = null
+}
+
+variable "content_encoding" {
+  type    = map(string)
+  default = null
+}
+
+variable "content_language" {
+  type    = map(string)
+  default = null
+}
+
+variable "content_type" {
+  type    = map(string)
+  default = null
+}
+
+variable "project" {
+  type    = string
+  default = null
+}
+
+variable "storage_class" {
+  type    = string
+  default = "STANDARD"
+}
+
+variable "lifecycle_rule" {
+  type = object({
+    action    = string
+    condition = string
+  })
+  default = null
+}
+
+variable "versioning" {
+  type    = bool
+  default = false
+}
+
+variable "force_destroy" {
+  type    = bool
+  default = true
+}
+
+variable "website" {
+  type = object({
+    main_page_suffix = string
+    not_found_page   = string
+  })
+  default = null
+}
+
+variable "cors" {
+  type = object({
+    origin          = list(string)
+    method          = list(string)
+    response_header = list(string)
+    max_age_seconds = number
+  })
+  default = null
 }

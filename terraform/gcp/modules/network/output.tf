3255ba91e438d5b1ce4e633b9be7fdf623142c27
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-11-14 11:10:05 +0900

    [mod] output.tfにkeyを追加

--- terraform/gcp/modules/network/output.tf
+++ terraform/gcp/modules/network/output.tf
@@ -1,36 +1,36 @@
 output "serverless_vpc_self_link" {
   value = {
-    for v in var.serverless_vpc : google_vpc_access_connector.main[v.name].self_link
+    for v in var.serverless_vpc : v.name => google_vpc_access_connector.main[v.name].self_link
   }
 }
 
 output "serverless_vpc_name" {
   value = {
-    for v in var.serverless_vpc : google_vpc_access_connector.main[v.name].name
+    for v in var.serverless_vpc : v.name => google_vpc_access_connector.main[v.name].name
   }
 }
 
 output "serverless_vpc_id" {
   value = {
-    for v in var.serverless_vpc : google_vpc_access_connector.main[v.name].id
+    for v in var.serverless_vpc : v.name => google_vpc_access_connector.main[v.name].id
   }
 }
 
 output "subnetwork_self_link" {
   value = {
-    for v in var.subnetworks : google_compute_subnetwork.main[v.name].self_link
+    for v in var.subnetworks : v.name => google_compute_subnetwork.main[v.name].self_link
   }
 }
 
 output "subnetwork_id" {
   value = {
-    for v in var.subnetworks : google_compute_subnetwork.main[v.name].id
+    for v in var.subnetworks : v.name => google_compute_subnetwork.main[v.name].id
   }
 }
 
 output "subnetwork_gateway_address" {
   value = {
-    for v in var.subnetworks : google_compute_subnetwork.main[v.name].gateway_address
+    for v in var.subnetworks : v.name => google_compute_subnetwork.main[v.name].gateway_address
   }
 }
 

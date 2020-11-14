1bf0638cc2059861b6d77669bd83e0975014af4a
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-11-14 12:29:07 +0900

    [mod] gce sampleを更新に合わせて修正

--- terraform/gcp/samples/gce.tf
+++ terraform/gcp/samples/gce.tf
@@ -1,39 +1,42 @@
-module "gce" {
-  depends_on = [
-    module.network,
-    module.iam,
-    module.service_account
-  ]
-  source = "../modules/gce"
+# module "gce" {
+#   depends_on = [
+#     module.network,
+#     module.iam,
+#     module.service_account
+#   ]
+#   source = "../modules/gce"
 
-  gce_conf = [
-    {
-      gce_enable = false
+#   gce_conf = [
+#     {
+#       gce_enable = false
 
-      name         = "test"
-      machine_type = "f1-micro"
-      zone         = local.zone
-      region       = local.region
-      tags         = ["test"]
-      subnetwork   = local.subnetwork.name
-      opt_conf = {
-        access_config = true
-        preemptible   = true
-      }
-      service_account = {
-        enable = true
+#       name         = "test"
+#       machine_type = "f1-micro"
+#       zone         = local.zone
+#       region       = local.region
+#       tags         = ["test"]
+#       subnetwork   = local.subnetwork.name
+#       opt_conf = {
+#         access_config           = true
+#         preemptible             = true
+#         metadata_startup_script = "set_vsftpd_conf"
+#       }
+#       service_account = {
+#         enable = true
 
-        email  = "module-sample"
-        scopes = []
-      }
-      boot_disk = {
-        size     = 10
-        type     = "pd-ssd"
-        opt_conf = {
-          image    = "ubuntu-2004-lts"
-        }
-      }
-    }
-  ]
-}
+#         email = "module-sample"
+#         scopes = [
+#           "https://www.googleapis.com/auth/cloud-platform"
+#         ]
+#       }
+#       boot_disk = {
+#         size = 20
+#         type = "pd-ssd"
+#         opt_conf = {
+#           image = "test-ftp-server"
+#         }
+#       }
+#     }
+#   ]
+# }
 

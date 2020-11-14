f29299f5112055fd5a0702253fdb37f6990cfef2
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-11-14 22:24:47 +0900

    [mod] kms モジュールをversion 0.13に合わせ修正

--- terraform/gcp/modules/kms/main.tf
+++ terraform/gcp/modules/kms/main.tf
@@ -1,36 +1,24 @@
-locals {
-  _key_ring_conf = flatten([
-    for _conf in var.kms_conf : {
-      name     = _conf.key_ring_conf.name
-      location = _conf.key_ring_conf.location
-    } if _conf.kms_enable
-  ])
-
-  _crypto_key_conf = flatten([
-    for _conf in var.kms_conf : [
-      for _crypto in _conf.crypto_key : {
-        name            = _crypto.name
-        key_ring        = _conf.key_ring_conf.name
-        prevent_destroy = _crypto.prevent_destroy
-      }
-    ] if _conf.crypto_enable
-  ])
-}
-
 resource "google_kms_key_ring" "main" {
-  depends_on = [google_project_service.main]
-  for_each   = { for v in local._key_ring_conf : v.name => v }
+  for_each = { for v in local._key_ring_conf : v.name => v }
 
   name     = each.value.name
   location = each.value.location
 }
 
 resource "google_kms_crypto_key" "main" {
-  for_each = { for v in local._crypto_key_conf : v.name => v }
+  for_each = { for v in var.keys : v.name => v }
 
-  name     = each.value.name
-  key_ring = google_kms_key_ring.main[each.value.key_ring].self_link
-  # lifecycle {
-  #   prevent_destroy = each.value.prevent_destroy
-  # }
+  name            = each.value.name
+  key_ring        = google_kms_key_ring.main.self_link
+  purpose         = var.purpose != null ? lookup(var.purpose, each.value.name, null) : null
+  rotation_period = var.rotation_period != null ? lookup(var.rotation_period, each.value.name, null) : null
+
+  dynamic "version_template" {
+    for_each = var.version_template != null ? compact([lookup(var.version_template, each.value.name, "")]) : []
+
+    content {
+      algorithm        = var.algorithm != null ? lookup(var.algorithm, each.value.name, "CRYPTO_KEY_VERSION_ALGORITHM_UNSPECIFIED") : "CRYPTO_KEY_VERSION_ALGORITHM_UNSPECIFIED"
+      protection_level = var.protection_level != null ? lookup(var.protection_level, each.value.name, null) : null
+    }
+  }
 }
--- terraform/gcp/modules/kms/variable.tf
+++ terraform/gcp/modules/kms/variable.tf
@@ -1,21 +1,30 @@
-variable "kms_conf" {
-  type = list(object({
-    kms_enable    = bool
-    crypto_enable = bool
-
-    key_ring_conf = object({
-      name     = string
-      location = string
-    })
-
-    crypto_key = list(object({
-      name            = string
-      prevent_destroy = bool
-    }))
-  }))
+variable "key_ring" {
+  type = object({
+    name     = string
+    location = string
+  })
 }
 
-variable "api_enable" {
-  type    = bool
-  default = false
+variable "keys" {
+  type = list(object({
+    name = string
+  }))
+}
+/*
+Option Configurations
+*/
+
+variable "project" {
+  type    = string
+  default = null
+}
+
+variable "algorithm" {
+  type    = map(string)
+  default = null
+}
+
+variable "protection_level" {
+  type    = map(string)
+  default = null
 }

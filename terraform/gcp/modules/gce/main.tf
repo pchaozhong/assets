d79d84b45710390b11d8218d2592c529da2c7ee7
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-11-14 12:16:20 +0900

    [mod] gce moduleをv0.13に対応するよう修正

--- terraform/gcp/modules/gce/main.tf
+++ terraform/gcp/modules/gce/main.tf
@@ -1,111 +1,101 @@
 locals {
-  _instance_conf_list = flatten([
-    for _conf in var.gce_conf : {
-      name                    = _conf.name
-      machine_type            = _conf.machine_type
-      zone                    = _conf.zone
-      tags                    = _conf.tags
-      subnetwork              = _conf.subnetwork
-      metadata_startup_script = lookup(_conf.opt_conf, "metadata_startup_script", null)
-      auto_delete             = lookup(_conf.boot_disk.opt_conf, "auto_delete", false)
-      device_name             = lookup(_conf.boot_disk.opt_conf, "device_name", null)
-      mode                    = lookup(_conf.boot_disk.opt_conf, "mode", "READ_WRITE")
-      disk_encryption_key_raw = lookup(_conf.boot_disk.opt_conf, "disk_encryption_key_raw", null)
-      scheduling              = lookup(_conf.opt_conf, "scheduling", false)
-      preemptible             = lookup(_conf.opt_conf, "preemptible", false)
-      automatic_restart       = lookup(_conf.opt_conf, "automatic_restart", false)
-      on_host_maintenance     = lookup(_conf.opt_conf, "on_host_maintenance", "TERMINATE")
-      access_config           = lookup(_conf.opt_conf, "access_config", true)
-      nat_ip                  = lookup(_conf.opt_conf, "nat_ip", null)
-      public_ptr_domain_name  = lookup(_conf.opt_conf, "public_ptr_domain_name", null)
-      network_tier            = lookup(_conf.opt_conf, "network_tier", null)
-      service_account = {
-        enable = _conf.service_account.enable
-        email  = _conf.service_account.enable == true ? data.google_service_account.main[_conf.service_account.email].email : null
-        scopes = _conf.service_account.scopes
-      }
-    } if _conf.gce_enable
-  ])
-
-  _boot_disk_conf_list = flatten([
-    for _conf in var.gce_conf : {
-      gce_name = _conf.name
-      name     = join("-", [_conf.name, "boot-disk"])
-      size     = _conf.boot_disk.size
-      type     = _conf.boot_disk.type
-      opt_conf = _conf.boot_disk.opt_conf
-      zone     = _conf.zone
-    } if _conf.gce_enable
-  ])
+  _service_account = var.service_account != null ? [
+    {
+      service_account = var.service_account
+      scopes          = var.scopes
+    }
+  ] : []
 }
 
 resource "google_compute_instance" "main" {
-  depends_on = [google_compute_disk.main]
-  for_each   = { for v in local._instance_conf_list : v.name => v }
+  name         = var.gce_instance.name
+  machine_type = var.gce_instance.machine_type
+  zone         = var.gce_instance.zone
+  project      = var.project
+  tags         = var.gce_instance.tags
 
-  name                    = each.value.name
-  machine_type            = each.value.machine_type
-  zone                    = each.value.zone
-  tags                    = each.value.tags
-  metadata_startup_script = each.value.metadata_startup_script
+  network_interface {
+    subnetwork = var.gce_instance.subnetwork
+    network_ip = var.private_ip
 
-  boot_disk {
-    auto_delete             = each.value.auto_delete
-    device_name             = each.value.device_name
-    mode                    = each.value.mode
-    disk_encryption_key_raw = each.value.disk_encryption_key_raw
-    source                  = google_compute_disk.main[each.value.name].self_link
-  }
+    dynamic "access_config" {
+      for_each = var.access_config ? ["enable"] : []
+      iterator = _conf
 
-  dynamic "scheduling" {
-    for_each = each.value.scheduling || each.value.preemptible ? [{
-      preemptible         = each.value.preemptible
-      automatic_restart   = each.value.automatic_restart
-      on_host_maintenance = each.value.on_host_maintenance
-    }] : []
-    content {
-      preemptible         = scheduling.value.preemptible
-      automatic_restart   = scheduling.value.automatic_restart
-      on_host_maintenance = scheduling.value.on_host_maintenance
+      content {
+        nat_ip                 = var.nat_ip
+        public_ptr_domain_name = var.public_ptr_domain_name
+        network_tier           = var.network_tier
+      }
     }
   }
 
-  network_interface {
-    subnetwork = data.google_compute_subnetwork.main[each.value.subnetwork].self_link
+  boot_disk {
+    auto_delete = var.boot_disk_auto_delete
+    device_name = var.boot_disk_device_name
+    source      = google_compute_disk.boot_disk.self_link
+  }
 
-    dynamic "access_config" {
-      for_each = each.value.access_config ? [{
-        nat_ip                 = each.value.nat_ip
-        public_ptr_domain_name = each.value.public_ptr_domain_name
-        network_tier           = each.value.network_tier
-      }] : []
-      content {
-        nat_ip                 = access_config.value.nat_ip
-        public_ptr_domain_name = access_config.value.public_ptr_domain_name
-        network_tier           = access_config.value.network_tier
-      }
+  dynamic "attached_disk" {
+    for_each = { for v in var.attached_disk : v.name => v }
+    iterator = _conf
+
+    content {
+      source      = google_compute_attached_disk.attached_disk[_conf.value.name].self_link
+      device_name = _conf.value.name
+      mode        = _conf.value.mode
+    }
+  }
+
+  dynamic "scheduling" {
+    for_each = var.scheduling ? ["enable"] : []
+
+    content {
+      preemptible         = var.preemptible
+      on_host_maintenance = var.on_host_maintenance
+      automatic_restart   = var.automatic_restart
     }
   }
 
   dynamic "service_account" {
-    for_each = each.value.service_account.enable ? [{
-      email  = each.value.service_account.email
-      scopes = each.value.service_account.scopes
-    }] : []
+    for_each = local._service_account
+    iterator = _conf
+
     content {
-      email  = service_account.value.email
-      scopes = service_account.value.scopes
+      email  = _conf.value.service_account
+      scopes = _conf.value.scopes
+    }
+  }
+
+  dynamic "guest_accelerator" {
+    for_each = var.accelerator != null ? [var.accelerator] : []
+    iterator = _conf
+
+    content {
+      type  = _conf.value.type
+      count = _conf.value.count
     }
   }
 }
 
-resource "google_compute_disk" "main" {
-  for_each = { for v in local._boot_disk_conf_list : v.gce_name => v }
+resource "google_compute_disk" "boot_disk" {
+  provider = google-beta
 
-  name  = each.value.name
-  size  = each.value.size
-  type  = each.value.type
-  zone  = each.value.zone
-  image = lookup(each.value.opt_conf, "image", null)
-  snapshot = lookup(each.value.opt_conf, "snapshot", null)
+  name      = var.boot_disk.name
+  size      = var.boot_disk.size
+  interface = var.boot_disk.interface
+  image     = var.boot_disk.image
+  zone      = var.gce_instance.zone
+  project   = var.project
+}
+
+resource "google_compute_disk" "attached_disk" {
+  for_each = { for v in var.attached_disk : v.name => v }
+  provider = google-beta
+
+  name      = each.value.name
+  size      = each.value.size
+  interface = each.value.interface
+  zone      = var.gce_instance.zone
+  project   = var.project
 }

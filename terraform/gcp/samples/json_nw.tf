ce348060a8f0639bc609cff42d68de22472ed915
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-11-20 12:30:37 +0900

    [add] json_nw moduleの追加

---
+++ terraform/gcp/samples/json_nw.tf
@@ -0,0 +1,11 @@
+locals {
+  json_network_sample_enable = true
+
+  _json_network_enable = local.json_network_sample_enable ? ["enable"] : []
+}
+
+module "json_network_sample" {
+  source = "../modules/file_modules/json_network"
+
+  file_path = "./files/network_object.json"
+}

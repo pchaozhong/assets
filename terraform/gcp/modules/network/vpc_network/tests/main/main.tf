3e1b74286b172476cb037ccd4c90ad0bd2ef4d19
AtsushiKitano <aquamarinearia1010@gmail.com>
2020-12-17 15:19:04 +0900

    [mod] testサンプルを複数のsubnetを作成するように修正

--- terraform/gcp/modules/network/vpc_network/tests/main/main.tf
+++ terraform/gcp/modules/network/vpc_network/tests/main/main.tf
@@ -3,27 +3,33 @@ locals {
 
   _insple_test = local.inspec_test_infra_enable ? [
     {
-      name   = "test"
-      cidr   = "192.168.0.0/29"
-      region = "asia-northeast1"
+      network = "test"
+      subnets = [
+        {
+          name   = "test-tokyo"
+          cidr   = "192.168.0.0/29"
+          region = "asia-northeast1"
+        },
+        {
+          name   = "test-osaka"
+          cidr   = "192.168.0.8/29"
+          region = "asia-northeast2"
+        }
+      ]
     }
   ] : []
 }
 
 module "network" {
-  for_each = { for v in local._insple_test : v.name => v }
+  for_each = { for v in local._insple_test : v.network => v }
   source   = "./terraform/gcp/modules/network/vpc_network"
 
   vpc_network = {
-    name = each.value.name
+    name = each.value.network
   }
 
   subnetworks = [
-    {
-      name   = each.value.name
-      cidr   = each.value.cidr
-      region = each.value.region
-    }
+    for v in each.value.subnetworks : v
   ]
 
   firewall = []
